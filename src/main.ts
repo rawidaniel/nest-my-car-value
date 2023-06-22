6a9eefd1c3b7d0f75fa90efdb3d332d67ca52ac1
rawidaniel <rawidaniel96@gmail.com>
2023-06-22 14:09:03 +0300

    my car value

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove all the properties that are not in the DTO
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
