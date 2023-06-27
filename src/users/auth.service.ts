ec1b67656d12a951b2e1d4fb9de415cd365293cc
rawidaniel <rawidaniel96@gmail.com>
2023-06-27 08:56:48 +0300

    add authentication for the application

---
+++ src/users/auth.service.ts
@@ -0,0 +1,60 @@
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // See if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+
+    // hash the user password
+
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex');
+
+    // hash the salt and password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // Join the hashed result and the salt together
+    const newPassword = salt + '.' + hash.toString('hex');
+
+    // Create a new user and save it
+    const user = await this.usersService.create(email, newPassword);
+
+    // return the user
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    // find the user with provided email
+    const [user] = await this.usersService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    // hash the provided password
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = hash.toString('hex');
+
+    if (result !== storedHash) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    return user;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,25 +8,68 @@ import {
   Delete,
   Patch,
   NotFoundException,
+  HttpCode,
+  Session,
+  UseGuards,
   UseInterceptors,
   ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UpdateUserDto } from './dtos/updateUser.dto';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { CurrentUser } from './decorators/currentUser.decorator';
+import { CurrentUserInterceptor } from './interceptors/currentUser.interceptor';
+import { AuthGuard } from './guards/auth.guard';
+import { User } from './user.entity';
 
 @Controller('auth')
+@Serialize(UserDto)
+// @UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  // @Get('/whoami')
+  // async whoAmI(@Session() session: any) {
+  //   const user = await this.usersService.findOne(session.userId);
+  //   if (!user) {
+  //     throw new NotFoundException('user not found');
+  //   }
+  //   return user;
+  // }
+
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  async whoAmI(@CurrentUser() user: User) {
+    return user;
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  @HttpCode(200)
+  async loginUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   // @UseInterceptors(new SerializeInterceptor(UserDto))
-  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     // console.log('handler is running');
